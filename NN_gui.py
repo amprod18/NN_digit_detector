8de707e5572c2b0271a021bb3e7faad4ef896382
amprod18 <martinezcuestaabraham@gmail.com>
2023-05-26 13:57:07 +0200

    gui kinda works

--- NN_gui.py
+++ NN_gui.py
@@ -5,6 +5,7 @@ import numpy as np
 import custom_NN
 import customtkinter as ctk
 from PIL import Image
+import keyboard
 
 
 class Main_menu(ctk.CTk):
@@ -38,20 +39,20 @@ class Main_menu(ctk.CTk):
         self.start_logo = ctk.CTkLabel(self.main_frame.tab("Loading Screen"), image=self.logo, text="")
         x = (self.screen_width - 20 - image_width) // 2
         y = (self.screen_height - 40 - image_height) // 2
-        f = lambda event: self.initial_progress_bar(event, x, y)
-        self.bind('<Button-1>', f)
-        self.bind('<Key>', f)
+
         self.start_logo.place(x=x, y=y)
 
         self.progressbar = ctk.CTkProgressBar(self.main_frame.tab("Loading Screen"), width=image_width, mode='determinate', determinate_speed=0.2)
         self.progressbar.set(0)
         y = y + image_height + 30
         
-        self.start_text = ctk.CTkLabel(self.main_frame.tab("Loading Screen"), width=image_width, height=50, text="Press Anywhere to Continue", fg_color='transparent', font=self.font)
+        self.start_text = ctk.CTkLabel(self.main_frame.tab("Loading Screen"), width=image_width, height=50, text="Press SPACE to Continue", fg_color='transparent', font=self.font)
         self.start_text.place(x=x-27, y=y)
 
-    def initial_progress_bar(self, event, x, y):
-        self.unbind_all(('<Button-1>', '<Key>'))
+        if keyboard.is_pressed('space'):
+            self.initial_progress_bar(x, y)
+
+    def initial_progress_bar(self, x, y):
         self.progressbar.place(x=x, y=y+50)
         self.progressbar.start()
 
@@ -83,6 +84,8 @@ class Main_menu(ctk.CTk):
         self.train_model_button = ctk.CTkButton(self.main_frame.tab("Main Menu"), text="Train Model", font=self.font)
         self.load_model_button.grid(row=0, column=0, padx=(20, 20), pady=(20, 20), sticky="nsew", rowspan=2)
         self.train_model_button.grid(row=0, column=1, padx=(20, 20), pady=(20, 20), sticky="nsew", rowspan=2)
+        self.train_model_button.configure(command=self.train_model)
+        self.load_model_button.configure(command=self.load_model)
 
         self.NN_info_frame = ctk.CTkFrame(self.main_frame.tab("Main Menu"))
         self.NN_info_frame.grid(row=3, column=0, padx=(20, 20), pady=(20, 20), sticky="nsew", columnspan=2, rowspan=10)
@@ -90,15 +93,16 @@ class Main_menu(ctk.CTk):
 
         self.predict_button = ctk.CTkButton(self.main_frame.tab("Main Menu"), text="Predict", font=self.font)
         self.predict_button.grid(row=13, column=0, padx=(20, 20), pady=(20, 20), sticky="nsew", columnspan=2, rowspan=2)
+        self.predict_button.configure(command=self.predict)
 
         # Configure NN Zone (center column)
         self.NN_image_frame = ctk.CTkFrame(self.main_frame.tab("Main Menu"))
         self.NN_image_frame.grid(row=1, column=2, padx=(20, 20), pady=(20, 20), sticky="nsew", columnspan=4, rowspan=12)
-        self.NN_image = ctk.CTkLabel(self.NN_image_frame, text="Neural Network image will appear here", fg_color='green')
+        self.NN_image = ctk.CTkLabel(self.NN_image_frame, text="Neural Network image will appear here", fg_color='green', font=self.font)
         self.NN_image.grid(row=0, column=0, padx=(20, 20), pady=(20, 20), sticky="nsew")
 
         # Configure Output Zone (right column)
-        self.input_image = ctk.CTkLabel(self.main_frame.tab("Main Menu"), text="Input image will appear here", fg_color='green')
+        self.input_image = ctk.CTkLabel(self.main_frame.tab("Main Menu"), text="Input image will appear here", fg_color='green', font=self.font)
         self.input_image.grid(row=0, column=6, padx=(20, 20), pady=(20, 20), sticky="nsew", columnspan=2, rowspan=6)
 
         self.output_info_frame = ctk.CTkFrame(self.main_frame.tab("Main Menu"))
@@ -108,12 +112,36 @@ class Main_menu(ctk.CTk):
         self.main_frame.set("Main Menu")
     
     def train_model(self):
-        self.NN_info_frame
-        return
+        learning_rate = float(self.NN_info_frame.learning_rate_entry.get())
+        self.hidden_layers_sizes = [int(i) for i in self.NN_info_frame.hidden_layers_size_entry.get().split(sep=', ')]
+
+        self.N_network = custom_NN.neural_network(self.hidden_layers_sizes, learning_rate)
+        self.N_network.train_mode(train_images, train_labels)
+        values = [learning_rate, self.hidden_layers_sizes, 784, 10, round(self.N_network.accuracy, 2), 'None']
+        self.NN_info_frame.update_input_frame(values)
     
     def load_model(self):
+        learning_rate = float(self.NN_info_frame.learning_rate_entry.get())
+        self.hidden_layers_sizes = [int(i) for i in self.NN_info_frame.hidden_layers_size_entry.get().split(sep=', ')]
 
-        return
+        self.N_network = custom_NN.neural_network(self.hidden_layers_sizes, learning_rate)
+        self.N_network.retrieve_params(cwd + "params_file")
+        values = [self.N_network.learning_rate, self.hidden_layers_sizes, 784, 10, self.N_network.accuracy, 'None']
+        self.NN_info_frame.update_input_frame(values)
+
+    def predict(self):
+        pred_image = self.NN_info_frame.input_path_entry.get()
+
+        data = np.random.randint(0, 1e4)
+        pred, pred_time = self.N_network.predict_mode(test_images[:, data], test_labels[data])
+
+        pred_image = ctk.CTkImage(light_image=Image.fromarray(test_images[:, data].reshape(28, 28)), size=(300, 300))
+        self.input_image.configure(image=pred_image, text='')
+
+        values = [self.N_network.learning_rate, self.hidden_layers_sizes, 784, 10, self.N_network.accuracy, pred_time]
+        self.NN_info_frame.update_input_frame(values)
+
+        self.output_info_frame.update_output_frame(self, self.N_network.activations[-1].reshape(1, 10), (pred, self.N_network.activations[-1][pred]))
     
     def exit_app(self):
         time.sleep(0.3)
@@ -131,10 +159,10 @@ class NN_info_frame(ctk.CTkFrame):
         self.frame_title = ctk.CTkLabel(master, text='Model Info', font=font)
         self.frame_title.grid(row=0, column=0, padx=(20, 20), pady=(20, 10), columnspan=2)
         # Hid layers 
-        self.hidden_layers_label = ctk.CTkLabel(master, text='Number of Hidden Layers: ', font=font)
-        self.hidden_layers_label.grid(row=1, column=0, padx=(20, 10), pady=(10, 10))
-        self.hidden_layers_entry = ctk.CTkEntry(master, placeholder_text='Sample: 2', font=font)
-        self.hidden_layers_entry.grid(row=1, column=1, padx=(10, 20), pady=(10, 10), sticky="ew")
+        self.learning_rate_label = ctk.CTkLabel(master, text='Learning Rate: ', font=font)
+        self.learning_rate_label.grid(row=1, column=0, padx=(20, 10), pady=(10, 10))
+        self.learning_rate_entry = ctk.CTkEntry(master, placeholder_text='Sample: 0.05', font=font)
+        self.learning_rate_entry.grid(row=1, column=1, padx=(10, 20), pady=(10, 10), sticky="ew")
 
         # Hid layers sizes
         self.hidden_layers_size_label = ctk.CTkLabel(master, text='Sizes of Hidden Layers: ', font=font)
@@ -167,24 +195,26 @@ class NN_info_frame(ctk.CTkFrame):
         self.pred_time_entry.grid(row=6, column=1, padx=(10, 20), pady=(10, 10))
 
         # Hid layers 
-        self.hidden_layers_label = ctk.CTkLabel(master, text='Input File Path: ', font=font)
-        self.hidden_layers_label.grid(row=7, column=0, padx=(20, 10), pady=(10, 10))
-        self.hidden_layers_entry = ctk.CTkEntry(master, placeholder_text='Path to File', font=font)
-        self.hidden_layers_entry.grid(row=7, column=1, padx=(10, 20), pady=(10, 20), sticky="ew")
+        self.input_path_label = ctk.CTkLabel(master, text='Input File Path: ', font=font)
+        self.input_path_label.grid(row=7, column=0, padx=(20, 10), pady=(10, 10))
+        self.input_path_entry = ctk.CTkEntry(master, placeholder_text='Path to File', font=font)
+        self.input_path_entry.grid(row=7, column=1, padx=(10, 20), pady=(10, 20), sticky="ew")
 
     def update_input_frame(self, values):
+        # Values = [learning_rate, hidden_layers_size, input_size, output_size, accuracy, pred_time]
+
         # Hid layers 
-        self.hidden_layers_entry.configure(placeholder_text=values[0])
+        self.learning_rate_entry.configure(placeholder_text=str(values[0]))
         # Hid layers sizes
-        self.hidden_layers_size_entry.configure(placeholder_text=values[1])
+        self.hidden_layers_size_entry.configure(placeholder_text=str(values[1]))
         # Input size
-        self.input_size_entry.configure(text=values[2])
+        self.input_size_entry.configure(text=str(values[2]))
         # Output size
-        self.output_size_entry.configure(text=values[3])
+        self.output_size_entry.configure(text=str(values[3]))
         # Accuracy
-        self.accuracy_entry.configure(text=values[4])
+        self.accuracy_entry.configure(text=str(values[4]))
         # Mean pred time
-        self.pred_time_entry.configure(text=values[5])
+        self.pred_time_entry.configure(text=str(values[5]))
 
 
 class NN_output_frame(ctk.CTkFrame):
@@ -206,6 +236,13 @@ class NN_output_frame(ctk.CTkFrame):
         
         self.pred_label = ctk.CTkLabel(master, text=f'Prediction: {pred[0]} with {pred[1]}% confidence', font=font)
         self.pred_label.grid(row=7, column=0, padx=(10, 10), pady=(10, 10), rowspan=2, sticky="nsew", columnspan=2)
+    
+    def update_output_frame(self, probabilities, pred):
+        for i in range(5):
+            self.probs_labels_1.configure(text=f'{i}: {probabilities[i]*100} %')
+            self.probs_labels_2.configure(text=f'{2*i+1}: {probabilities[2*i+1]*100} %')
+
+        self.pred_label.configure(text=f'Prediction: {pred[0]} with {pred[1]*100}% confidence')
              
 
 def read_data(filename_images, filename_labels):
@@ -241,22 +278,16 @@ def load_data(filename_images_train, filename_labels_train, filename_images_test
 
 
 if __name__ == "__main__":
-    global cwd 
+    global cwd, train_images, train_labels, test_images, test_labels
     cwd = os.getcwd() + "\\"
 
     ctk.set_appearance_mode("System")
     ctk.set_default_color_theme("blue")
 
+    train_images, train_labels, test_images, test_labels = load_data("training_set/train-images-idx3-ubyte.gz", "training_set/train-labels-idx1-ubyte.gz", "test_set/t10k-images-idx3-ubyte.gz", "test_set/t10k-labels-idx1-ubyte.gz")
+
     Gui = Main_menu()
 
     Gui.mainloop()
 
-    train_images, train_labels, test_images, test_labels = load_data("training_set/train-images-idx3-ubyte.gz", "training_set/train-labels-idx1-ubyte.gz", "test_set/t10k-images-idx3-ubyte.gz", "test_set/t10k-labels-idx1-ubyte.gz")
-
-    learning_rate = 0.05
-    # N_network = custom_NN.neural_network([10, 10], learning_rate)
-    # N_network.train_mode(train_images, train_labels)
-    # N_network.save_params("params_file")
-    # N_network.retrieve_params(cwd + "params_file")
-    #data = np.random.randint(0, 1e4)
-    # N_network.predict_mode(test_images[:, data], test_labels[data])
+    # self.N_network.save_params("params_file")
